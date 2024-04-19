81124f761347a682d424a0a2eb6c47dbfc46323b
Konstantin Demidov <88594050+KoldKilla@users.noreply.github.com>
2024-04-19 22:31:09 +0300

    Update ConfProp.java

--- src/main/java/pages/ConfProp.java
+++ src/main/java/pages/ConfProp.java
@@ -13,7 +13,7 @@ public class ConfProp {
 
     static {
         try {
-            fileInputStream = new FileInputStream("src/main/resources/conf.properties");
+            fileInputStream = new FileInputStream("src/main/java/pages/ConfProp");
             PROPERTIES = new Properties();
             PROPERTIES.load(fileInputStream);
         } catch (Throwable e) {
