649981b4a99b125c61c31a7b9fbe72b9c3171939
Konstantin Demidov <88594050+KoldKilla@users.noreply.github.com>
2024-04-22 01:20:04 +0300

    Update TestRunner.java

--- src/test/java/org/ibs/runner/TestRunner.java
+++ src/test/java/org/ibs/runner/TestRunner.java
@@ -12,7 +12,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @ConfigurationParameters({
-        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@ui"),
+        @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
         @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.ibs.steps" ),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
